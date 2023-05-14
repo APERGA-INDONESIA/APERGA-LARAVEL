5bbd9718cd6043210c0cec7497746368292525a3
moylie <adhityapmp@gmail.com>
2023-05-14 09:21:47 +0700

    add detailpekerja

---
+++ resources/views/detailpekerja.blade.php
@@ -0,0 +1,15 @@
+@extends('template')
+@section('title', 'Detail Pekerja')
+@section('navbar')
+@endsection
+
+@section('konten')
+
+@endsection
+
+@section('footer')
+@endsection
+
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/XXXX.css') }}">
+@endpush
