75d95e810f2a6e0b23c52e73b122beb0b8a68ed6
moylie <adhityapmp@gmail.com>
2023-05-15 00:48:56 +0700

    add detailpekerja

--- resources/views/detailpekerja.blade.php
+++ resources/views/detailpekerja.blade.php
@@ -1,15 +1,32 @@
 @extends('template')
+
 @section('title', 'Detail Pekerja')
+
 @section('navbar')
 @endsection
 
 @section('konten')
+<div class="kotak-hitam">
+    <div class="detail-pekerja">
+        Detail Pekerja
+    </div>
+</div>
 
+<div class="infopekerja">
+    <div class="big-profile">
+        <img src="{{ asset('images/bigprofile.png') }}" alt="Big Profile" class="big-profile-img">
+    </div>
+    <div class="deskripsi">
+        <div class="nama-pekerja">Abdul Ghafur</div>
+        <!-- Isi deskripsi di sini -->
+    </div>
+</div>
 @endsection
 
 @section('footer')
 @endsection
 
 @push('styles')
-    <link rel="stylesheet" href="{{ asset('css/XXXX.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/detailpekerja.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
 @endpush
