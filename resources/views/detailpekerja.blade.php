a9135b80cef590f4b1f0ec47226d80ab2cc31ff0
moylie <adhityapmp@gmail.com>
2023-05-17 22:34:19 +0700

    update detailpekerja

--- resources/views/detailpekerja.blade.php
+++ resources/views/detailpekerja.blade.php
@@ -1,5 +1,4 @@
 @extends('template')
-
 @section('title', 'Detail Pekerja')
 
 @section('navbar')
@@ -18,7 +17,24 @@
     </div>
     <div class="deskripsi">
         <div class="nama-pekerja">Abdul Ghafur</div>
-        <!-- Isi deskripsi di sini -->
+        <div class="rating">
+            <img src="{{ asset('images/star.png') }}" alt="Star" class="star-img">
+            <span class="rating-text">4.5/5</span>
+        </div>
+        <div class="deskripsi-jam-kerja">
+            <div class="jam-kerja">
+                <div class="jam-kerja-title">Jam Kerja</div>
+                <div class="jam-kerja-box">
+                    <div class="jam-kerja-text">4 jam/hari</div>
+                </div>
+            </div>
+            <div class="durasi-kerja">
+                <div class="durasi-kerja-title">Durasi Kerja</div>
+                <div class="durasi-kerja-box">
+                    <div class="durasi-kerja-text">1 Bulan</div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
