348ea357c00e52e047f7b37fe0907fc0e98d19db
Bayu Liano <bayuliano30@gmail.com>
2023-05-21 23:38:10 +0700

    update cariPRT

--- resources/views/cariPRT.blade.php
+++ resources/views/cariPRT.blade.php
@@ -7,7 +7,13 @@
 <div class="kotak-utama">
         <div class="header"><img src="{{ asset('images/arrow-white.png') }}" alt="Arrow" class="arrow"></div>
         <div class="header"><p >Pilihan Pekerja di Aperga</p></div>
+
 </div>
+<form class="search-box">
+    <div><input class="search-input" type="text" name="cari" placeholder="Cari Pekerja"></div>
+    <div><input class="search-button" id="cari-text" type="submit" value="Cari"></div>
+</form>
+
 
 @endsection
 
