bec4bcc4f1097a9b04aa3f73c495df629eb865a9
moylie <adhityapmp@gmail.com>
2023-05-26 18:04:32 +0700

    update dashbor fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,11 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->decimal('saldo', 10, 2)->default(0);
+            $table->integer('poin')->default(0);
         });
+
+
     }
 
     /**
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,20 +7,43 @@
 @endsection
 
 @section('konten')
+
 <div class="dashboard-kotak">
-    <div class="search-container">
-        <img src="{{ asset('images/search.png') }}" alt="Search" class="search-icon">
-        <input type="text" class="cari-input" placeholder="Cari PRT">
-    </div>
-</div>
-
-
-<div class="dashboard-kotak-2">
     <a href="http://localhost:8000/profil">
         <img src="{{ asset('images/profile 1.png') }}" alt="Profile Image" class="profile-img">
         <p class="greeting">Halo, Selamat Datang</p>
         <p class="username">{{ Auth::user()->name }}</p>
     </a>
+</div>
+
+
+<div class="dashboard-kotak-2">
+    <p class="Pointext">Total Poin Saya</p>
+    <img src="{{ asset('images/coin.png') }}" alt="Poin" class="MyPoin">
+
+    @php
+    $user = auth()->user();
+    $totalPoin = $user->poin;
+    @endphp
+
+    <p class="TotalPoin">{{ $totalPoin }} AP Point</p>
+    <button class="redeem" onclick="location.href='http://localhost:8000/redeem'">
+        <p class="redeemtext">Redeem</p>
+    </button>
+</div>
+
+<div class="dashboard-kotak-4">
+    <p class="Saldotext">Total Saldo Saya</p>
+    <img src="{{ asset('images/saldo.png') }}" alt="Poin" class="MyPoin">
+    @php
+    $user = auth()->user();
+    $saldo = $user->saldo;
+    @endphp
+
+    <p class="TotalSaldo">Rp.{{ $saldo }}</p>
+    <button class="isiulang" onclick="location.href='http://localhost:8000/topup'">
+        <p class="isiulangtext">Isi Ulang</p>
+    </button>
 </div>
 
 <div class="dashboard-kotak-3">
