968a7336058c89b0a73e7cff322c05f2d880c1c6
moylie <adhityapmp@gmail.com>
2023-05-29 01:53:52 +0700

    update detail-pekerja (FE)

--- resources/views/detail-pekerja.blade.php
+++ resources/views/detail-pekerja.blade.php
@@ -11,7 +11,7 @@
     </div>
 </div>
 
-<div class="infopekerja">
+<div class="info-pekerja">
     <div class="big-profile">
         <img src="{{ asset('images/bigprofile.png') }}" alt="Big Profile" class="big-profile-img">
     </div>
@@ -22,6 +22,16 @@
             <span class="rating-text">4.5/5</span>
         </div>
 
+        <div class="infopekerja">
+            <p class="lokasi"><strong>Lokasi Pekerja:</strong> Surabaya</p>
+            <p class="gaji"><strong>Besaran Gaji:</strong> Rp 5.000.000,00</p>
+            <p class="umur"><strong>Umur Pekerja:</strong> 30 Tahun</p>
+        </div>
+
+        <div class="deskripsi-pekerja">
+            <p class="deskripsi-text"><strong>Deskripsi:</strong></p>
+            <p class="isi-deskripsi">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+        </div>
 
         <div class="button-group">
             <button class="WABtn" onClick="window.location.href = 'http://localhost:8000/waadmin';">
@@ -30,12 +40,10 @@
             <button class="order" onClick="window.location.href = 'http://localhost:8000/mulaikontrak';">
               <span>Pesan Sekarang</span>
             </button>
-          </div>
+        </div>
 
     </div>
 </div>
-
-
 
 @endsection
 
